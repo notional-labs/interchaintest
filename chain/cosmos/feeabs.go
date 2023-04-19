a38fbcca50f2e1238dcb22dd648b2dd721578c6e
GnaD <cpt.meoz@gmail.com>
2023-04-19 14:38:16 +0700

    add return

--- chain/cosmos/feeabs.go
+++ chain/cosmos/feeabs.go
@@ -2,16 +2,15 @@ package cosmos
 
 import (
 	"context"
-	"fmt"
 )
 
 func FeeabsCrossChainSwap(c *CosmosChain, ctx context.Context, keyName string, ibcDenom string) error {
 	tn := c.getFullNode()
 
-	if _, err := tn.ExecTx(ctx, keyName,
+	_, err := tn.ExecTx(ctx, keyName,
 		"swap", ibcDenom,
 		"--pool-file", "--gas", "auto",
-	); err != nil {
-		return fmt.Errorf("failed to swap: %w", err)
-	}
+	)
+
+	return err
 }
