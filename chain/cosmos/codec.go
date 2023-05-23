44d4c6388f47fc1c3846ada25c88f8451ecc19f9
Lặc <expertdicer@gmail.com>
2023-05-23 17:05:11 +0700

    he

--- chain/cosmos/codec.go
+++ chain/cosmos/codec.go
@@ -7,51 +7,32 @@ import (
 	"github.com/cosmos/cosmos-sdk/types/module/testutil"
 	"github.com/cosmos/cosmos-sdk/x/auth"
 	authTx "github.com/cosmos/cosmos-sdk/x/auth/tx"
-	"github.com/cosmos/cosmos-sdk/x/bank"
-	"github.com/cosmos/cosmos-sdk/x/capability"
-	"github.com/cosmos/cosmos-sdk/x/consensus"
-	distr "github.com/cosmos/cosmos-sdk/x/distribution"
-	"github.com/cosmos/cosmos-sdk/x/genutil"
-	genutiltypes "github.com/cosmos/cosmos-sdk/x/genutil/types"
-	"github.com/cosmos/cosmos-sdk/x/gov"
-	govclient "github.com/cosmos/cosmos-sdk/x/gov/client"
-	"github.com/cosmos/cosmos-sdk/x/mint"
-	"github.com/cosmos/cosmos-sdk/x/params"
-	paramsclient "github.com/cosmos/cosmos-sdk/x/params/client"
-	"github.com/cosmos/cosmos-sdk/x/slashing"
-	"github.com/cosmos/cosmos-sdk/x/staking"
-	"github.com/cosmos/cosmos-sdk/x/upgrade"
-	upgradeclient "github.com/cosmos/cosmos-sdk/x/upgrade/client"
-
-	transfer "github.com/cosmos/ibc-go/v7/modules/apps/transfer"
-	ibccore "github.com/cosmos/ibc-go/v7/modules/core"
-	ibcwasm "github.com/strangelove-ventures/interchaintest/v7/chain/cosmos/08-wasm-types"
 )
 
 func DefaultEncoding() testutil.TestEncodingConfig {
 	return testutil.MakeTestEncodingConfig(
 		auth.AppModuleBasic{},
-		genutil.NewAppModuleBasic(genutiltypes.DefaultMessageValidator),
-		bank.AppModuleBasic{},
-		capability.AppModuleBasic{},
-		staking.AppModuleBasic{},
-		mint.AppModuleBasic{},
-		distr.AppModuleBasic{},
-		gov.NewAppModuleBasic(
-			[]govclient.ProposalHandler{
-				paramsclient.ProposalHandler,
-				upgradeclient.LegacyProposalHandler,
-				upgradeclient.LegacyCancelProposalHandler,
-			},
-		),
-		params.AppModuleBasic{},
-		slashing.AppModuleBasic{},
-		upgrade.AppModuleBasic{},
-		consensus.AppModuleBasic{},
-		transfer.AppModuleBasic{},
-		ibccore.AppModuleBasic{},
-		// ibctm.AppModuleBasic{},
-		ibcwasm.AppModuleBasic{},
+		// genutil.NewAppModuleBasic(genutiltypes.DefaultMessageValidator),
+		// bank.AppModuleBasic{},
+		// capability.AppModuleBasic{},
+		// staking.AppModuleBasic{},
+		// mint.AppModuleBasic{},
+		// distr.AppModuleBasic{},
+		// gov.NewAppModuleBasic(
+		// 	[]govclient.ProposalHandler{
+		// 		paramsclient.ProposalHandler,
+		// 		upgradeclient.LegacyProposalHandler,
+		// 		upgradeclient.LegacyCancelProposalHandler,
+		// 	},
+		// ),
+		// params.AppModuleBasic{},
+		// slashing.AppModuleBasic{},
+		// upgrade.AppModuleBasic{},
+		// consensus.AppModuleBasic{},
+		// transfer.AppModuleBasic{},
+		// ibccore.AppModuleBasic{},
+		// // ibctm.AppModuleBasic{},
+		// ibcwasm.AppModuleBasic{},
 	)
 }
 
