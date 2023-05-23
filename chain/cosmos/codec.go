b20a116e2eb7fa99ea65743ba4ed389546df981c
Lặc <expertdicer@gmail.com>
2023-05-23 17:17:06 +0700

    test

--- chain/cosmos/codec.go
+++ chain/cosmos/codec.go
@@ -7,24 +7,35 @@ import (
 	"github.com/cosmos/cosmos-sdk/types/module/testutil"
 	"github.com/cosmos/cosmos-sdk/x/auth"
 	authTx "github.com/cosmos/cosmos-sdk/x/auth/tx"
+	"github.com/cosmos/cosmos-sdk/x/bank"
+	"github.com/cosmos/cosmos-sdk/x/capability"
+	distr "github.com/cosmos/cosmos-sdk/x/distribution"
+	"github.com/cosmos/cosmos-sdk/x/genutil"
+	genutiltypes "github.com/cosmos/cosmos-sdk/x/genutil/types"
+	"github.com/cosmos/cosmos-sdk/x/gov"
+	govclient "github.com/cosmos/cosmos-sdk/x/gov/client"
+	"github.com/cosmos/cosmos-sdk/x/mint"
+	paramsclient "github.com/cosmos/cosmos-sdk/x/params/client"
+	"github.com/cosmos/cosmos-sdk/x/staking"
+	upgradeclient "github.com/cosmos/cosmos-sdk/x/upgrade/client"
 )
 
 func DefaultEncoding() testutil.TestEncodingConfig {
 	return testutil.MakeTestEncodingConfig(
 		auth.AppModuleBasic{},
-		// genutil.NewAppModuleBasic(genutiltypes.DefaultMessageValidator),
-		// bank.AppModuleBasic{},
-		// capability.AppModuleBasic{},
-		// staking.AppModuleBasic{},
-		// mint.AppModuleBasic{},
-		// distr.AppModuleBasic{},
-		// gov.NewAppModuleBasic(
-		// 	[]govclient.ProposalHandler{
-		// 		paramsclient.ProposalHandler,
-		// 		upgradeclient.LegacyProposalHandler,
-		// 		upgradeclient.LegacyCancelProposalHandler,
-		// 	},
-		// ),
+		genutil.NewAppModuleBasic(genutiltypes.DefaultMessageValidator),
+		bank.AppModuleBasic{},
+		capability.AppModuleBasic{},
+		staking.AppModuleBasic{},
+		mint.AppModuleBasic{},
+		distr.AppModuleBasic{},
+		gov.NewAppModuleBasic(
+			[]govclient.ProposalHandler{
+				paramsclient.ProposalHandler,
+				upgradeclient.LegacyProposalHandler,
+				upgradeclient.LegacyCancelProposalHandler,
+			},
+		),
 		// params.AppModuleBasic{},
 		// slashing.AppModuleBasic{},
 		// upgrade.AppModuleBasic{},
